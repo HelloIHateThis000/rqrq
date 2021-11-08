370c2076c313ca303b1eef3643a470db97bf6115
HelloIHateThis000 <93497090+HelloIHateThis000@users.noreply.github.com>
2021-11-07 16:46:31 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+song=""
 function preload({
-    
+song=loadSound("music.mp3")
 })
 
 function setup(){
-    canvas=canvas.Create()
+    canvas=canvas.Create();
+    canvas.center();
+    video=create.Capture(VIDEO);
+    video.hide
+}
+
+function draw(){
+    image(video,0,0,600,500);
+
+}
+
+function play(){
+    song.play();
 }
